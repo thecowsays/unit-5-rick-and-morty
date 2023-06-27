53941ed5dec0f6e39ca2ceaf237b10d7f0c0c639
thecowsays <117232523+thecowsays@users.noreply.github.com>
2023-06-27 00:29:25 -0400

    wip - updated App files

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import './App.css'
+import Header from './components/Header/Header'
+import Homepage from './pages/Homepage/Homepage'
+import Footer from './components/Footer/Footer'
 
 function App() {
 
   return (
-    <>
-    </>
+    <div>
+      <Header />
+      <Homepage />
+      <Footer />
+    </div>
   )
 }
 
