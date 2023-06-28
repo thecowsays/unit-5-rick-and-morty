0ff35c0b38e2c8a248107e57c5833bafc868fb80
thecowsays <117232523+thecowsays@users.noreply.github.com>
2023-06-27 21:13:22 -0400

    updated Header jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,8 +5,8 @@ function Header() {
     return (
         <div className="header-container">
             <div>
-                <a href="#">Home</a>
-                <a href="#">About</a>
+                <a href="#" style={{ marginRight: "10px" }}>Home</a>
+                <a href="#" style={{ marginRight: "10px" }}>About</a>
                 <a href="#">Episodes</a>
             </div>
             <button className="theme-button">Dark Mode</button>
