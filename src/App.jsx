9359bfb89b7cb0c1ba68a42e921f3b683966ff87
thecowsays <117232523+thecowsays@users.noreply.github.com>
2023-07-01 23:10:11 -0400

    imported About, Episodes; added Routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,25 @@ import React from 'react'
 import './App.css'
 import Header from './components/Header/Header'
 import Homepage from './pages/Homepage/Homepage'
+import About from './pages/About/About'
+import Episodes from './pages/Episodes/Episodes'
 import Footer from './components/Footer/Footer'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
 
   return (
-    <div>
+    <BrowserRouter>
       <Header />
-      <Homepage />
+
+      <Routes>
+        <Route path='/' element={<Homepage />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/episodes' element={<Episodes />} />
+      </Routes>
+
       <Footer />
-    </div>
+    </BrowserRouter>
   )
 }
 
