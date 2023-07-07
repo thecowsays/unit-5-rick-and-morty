3a8e9de7db343ae9bfb36087f82f53d6330cab1b
thecowsays <117232523+thecowsays@users.noreply.github.com>
2023-07-06 22:45:18 -0400

    wip added css, imported

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Header from './components/Header/Header'
 import Homepage from './pages/Homepage/Homepage'
 import About from './pages/About/About'
 import Episodes from './pages/Episodes/Episodes'
 import Footer from './components/Footer/Footer'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import CharacterDetails from './pages/CharacterDetails/CharacterDetails'
 
 function App() {
 
@@ -17,6 +18,7 @@ function App() {
         <Route path='/' element={<Homepage />} />
         <Route path='/about' element={<About />} />
         <Route path='/episodes' element={<Episodes />} />
+        <Route path='/details/:characterId' element={<CharacterDetails />} />
       </Routes>
 
       <Footer />
--- src/pages/CharacterDetails/CharacterDetails.jsx
+++ src/pages/CharacterDetails/CharacterDetails.jsx
@@ -1,9 +1,51 @@
 import React from 'react'
-import './CharacterDetails'
+import './CharacterDetails.css'
+import { useParams } from 'react-router-dom'
+import axios from 'axios'
 
 function CharacterDetails() {
+
+    // this page shows details of a specific character
+    // what character ?
+    // the id is in the url
+    // get the parameter
+
+    const { characterId } = useParams()
+
+    // create state to hold data from api
+    const [character, setCharacter] = React.useState('')
+
+
+    // I want to see character details when page loads
+    // https://rickandmortyapi.com/api/character/2
+
+    React.useEffect(
+        () => {
+            //make api call to get the data
+            axios.get(`https://rickandmortyapi.com/api/character/${characterId}`)
+                .then(res => {
+                    console.log(res.data)
+                    // I have the data, what do I do with it?
+                    // store it in state
+                    setCharacter(res.data)
+                })
+                .catch(err => console.log(err))
+
+        }, [] // dependency array
+    )
+
     return (
-        <div>CharacterDetails</div>
+        <div className="details-container">
+            <img src={character?.image} />
+            <div className="container-info">
+                <p>Name: {character?.name}</p>
+                <p>Gender: {character?.gender}</p>
+                <p>Location: {character?.location?.name}</p>
+                <p>Species: {character?.species}</p>
+            </div>
+
+
+        </div>
     )
 }
 
