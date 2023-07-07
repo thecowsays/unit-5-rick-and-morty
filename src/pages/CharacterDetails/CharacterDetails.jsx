1298f0d08886a9d3367cbc4e71d013af7d7f60c9
thecowsays <117232523+thecowsays@users.noreply.github.com>
2023-07-06 21:31:34 -0400

    created CharacterDetails page

---
+++ src/pages/CharacterDetails/CharacterDetails.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './CharacterDetails'
+
+function CharacterDetails() {
+    return (
+        <div>CharacterDetails</div>
+    )
+}
+
+export default CharacterDetails
