f99002716d01aa3b85e03de575a009e05770f249
thecowsays <117232523+thecowsays@users.noreply.github.com>
2023-07-24 19:37:48 -0400

    added dark mode / light mode functionality

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,14 +2,25 @@ import React from 'react'
 import './Header.css'
 
 function Header() {
+    // create variable to control darkMode
+    // const darkMode = false;
+
+    // create state to control darkMode
+    const [darkMode, setDarkMode] = React.useState(false)
+
     return (
-        <div className="header-container">
+        <div className={darkMode ? "header-container header-dark" : "header-container"}>
             <div>
                 <a href="/" style={{ marginRight: "10px" }}>Home</a>
                 <a href="/about" style={{ marginRight: "10px" }}>About</a>
                 <a href="/episodes">Episodes</a>
             </div>
-            <button className="theme-button">Dark Mode</button>
+            <button className={darkMode ? "theme-button theme-button-dark" : "theme-button"}
+                onClick={() => setDarkMode(!darkMode)}>
+                {
+                    darkMode ? "Light Mode" : "Dark Mode"
+                }
+            </button>
         </div>
     )
 }
