97a3a578975761b11d1fb1260f1e1e5d5d49665b
thecowsays <117232523+thecowsays@users.noreply.github.com>
2023-07-24 19:06:22 -0400

    wip - lesson 4 code-a-long

--- src/pages/Episodes/Episodes.jsx
+++ src/pages/Episodes/Episodes.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Episodes.css'
+import axios from 'axios'
 
 function Episodes() {
+
+    // when I select an episode, I want to see the chars within same
+    // https://rickandmortyapi.com/api/episode
+    // I need to create the UI, how many eps when page loads?
+    useEffect(
+        () => {
+            //make api call to find out # of eps
+            axios.get(`https://rickandmortyapi.com/api/episode`)
+                .then(res => {
+                    console.log(res.data.info.count)
+                    // create an array of numbers from 1 to this value
+                    const newOptions = [];
+                    for (let i = 1; i <= res.data.info.count; i++) {
+                        newOptions.push(i)
+                    }
+                    const [newOptions, setOptions] = useState([])
+                    console.log(newOptions)
+                    setOptions(newOptions)
+                })
+                .catch(err => console.log(err))
+        }
+    )
+
+
     return (
-        <div>Episodes</div>
+        <div className="episodes-container">
+            <div>
+                <label htmlFor="select-episode">Select an episode</label>
+                <select id="select-episode">
+
+                    {
+                        options.map(num => <option>{`Episode ${num}`}</option>)
+                    }
+
+                </select>
+                <div>
+                    Episode data goes here
+                </div>
+            </div>
+        </div>
     )
 }
 
