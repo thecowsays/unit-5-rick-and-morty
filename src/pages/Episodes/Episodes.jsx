4c436d14f4993a6f195ba7ee137cfb64d87dd6c8
thecowsays <117232523+thecowsays@users.noreply.github.com>
2023-08-08 23:56:34 -0400

    wip - adding episode listbox

--- src/pages/Episodes/Episodes.jsx
+++ src/pages/Episodes/Episodes.jsx
@@ -7,35 +7,45 @@ function Episodes() {
     // when I select an episode, I want to see the chars within same
     // https://rickandmortyapi.com/api/episode
     // I need to create the UI, how many eps when page loads?
+
+    // create state to hold the new array
     const [options, setOptions] = useState([])
+    // create state for option selected by user
+    const [selectedOption, setSelectedOption] = useState(1)
 
     useEffect(
         () => {
             //make api call to find out # of eps
             axios.get(`https://rickandmortyapi.com/api/episode`)
                 .then(res => {
-                    console.log(res.data.info.count)
+                    // console.log(res.data.info.count)
                     // create an array of numbers from 1 to this value
+                        // so you can use .push
                     const newOptions = [];
                     for (let i = 1; i <= res.data.info.count; i++) {
                         newOptions.push(i)
                     }
-                    console.log(newOptions)
+                    // console.log(newOptions)
                     setOptions(newOptions)
                 })
                 .catch(err => console.log(err))
-        }
+        }, [] // run once, dependency array
     )
 
+    const handleSelectEpisode = (e) => {
+        // console.log('select episode')
+        // console.log(e.target.value)
+        setSelectedOption(e.target.value)
+    }
 
     return (
         <div className="episodes-container">
             <div>
-                <label htmlFor="select-episode">Select an episode</label>
-                <select id="select-episode">
+                <label htmlFor="select-episode">Select an episode: </label>
+                <select id="select-episode" onChange={handleSelectEpisode}>
 
                     {
-                        options.map(num => <option>{`Episode ${num}`}</option>)
+                        options.map(num => <option key={num} value={num}>{`Episode ${num}`}</option>)
                     }
 
                 </select>
